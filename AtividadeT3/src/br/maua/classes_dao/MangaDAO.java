da6d02e26ce28965bf08510f5952d27eba513e2e
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-09-21 12:12:22 -0300

    Update MangaDAO

--- AtividadeT3/src/br/maua/classes_dao/MangaDAO.java
+++ AtividadeT3/src/br/maua/classes_dao/MangaDAO.java
@@ -4,9 +4,9 @@ import br.maua.dao.DAO;
 import br.maua.dao.DAOFields;
 import br.maua.model.Manga;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MangaDAO implements DAO<Manga>, DAOFields {
@@ -23,46 +23,106 @@ public class MangaDAO implements DAO<Manga>, DAOFields {
 
     @Override
     public List<Manga> get(String condition) {
-        return null;
+        List<Manga> mangas = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getSelectConditionalString(getTableName()) + condition);
+            while (resultSet.next()) {
+                Manga manga = new Manga(
+                        resultSet.getString("nome"),
+                        resultSet.getString("url"),
+                        resultSet.getString("sinopse"),
+                        resultSet.getInt("capitulos"),
+                        resultSet.getInt("volumes"),
+                        resultSet.getString("tipo"),
+                        resultSet.getDouble("nota")
+                );
+                mangas.add(manga);
+            }
+            resultSet.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mangas;
     }
 
     @Override
     public List<Manga> getAll() {
-        return null;
+        List<Manga> mangas = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getSelectAllString(getTableName()));
+            while (resultSet.next()) {
+                Manga manga = new Manga(
+                        resultSet.getString("nome"),
+                        resultSet.getString("url"),
+                        resultSet.getString("sinopse"),
+                        resultSet.getInt("capitulos"),
+                        resultSet.getInt("volumes"),
+                        resultSet.getString("tipo"),
+                        resultSet.getDouble("nota")
+                );
+                mangas.add(manga);
+            }
+            resultSet.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mangas;
     }
 
     @Override
     public void delete(Manga manga) {
-
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getDeleteString(getTableName()));
+            preparedStatement.setString(1, manga.getNome());
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void create(Manga manga) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getInsertString(getTableName()));
+            preparedStatement.setString(1, manga.getNome());
+            preparedStatement.setString(2, manga.getUrl());
+            preparedStatement.setString(3, manga.getSinopse());
+            preparedStatement.setInt(4, manga.getCapitulos());
+            preparedStatement.setInt(5, manga.getVolumes());
+            preparedStatement.setString(6, manga.getTipo());
+            preparedStatement.setDouble(7, manga.getNota());
 
+            int retorno = preparedStatement.executeUpdate();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getTableName() {
-        return null;
+        return "manga";
     }
 
     @Override
     public String getDeleteString(String table) {
-        return null;
+        return "DELETE FROM " + table + " WHERE nome = ?";
     }
 
     @Override
-    public String getInsertString(String tabel) {
-        return null;
+    public String getInsertString(String table) {
+        return "INSERT INTO " + table + " (nome, url, sinopse, capitulos, volumes, tipo, nota) VALUES (?, ?, ?, ?, ?, ?, ?);";
     }
 
     @Override
     public String getSelectAllString(String table) {
-        return null;
+        return "SELECT * FROM " + table;
     }
 
     @Override
     public String getSelectConditionalString(String table) {
-        return null;
+        return "SELECT * FROM " + table + " WHERE ";
     }
 }
--- AtividadeT3/src/br/maua/dao/DAOFields.java
+++ AtividadeT3/src/br/maua/dao/DAOFields.java
@@ -3,7 +3,7 @@ package br.maua.dao;
 public interface DAOFields {
     String getTableName();
     String getDeleteString(String table);
-    String getInsertString(String tabel);
+    String getInsertString(String table);
     String getSelectAllString(String table);
     String getSelectConditionalString(String table);
 }
