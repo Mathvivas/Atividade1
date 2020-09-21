c3d94556ce05e0d32167fef1979037856ac5e93b
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-09-21 11:08:29 -0300

    Update AnimeDAO
    - Implementação das Interfaces DAO.

--- AtividadeT3/src/br/maua/classes_dao/AnimeDAO.java
+++ AtividadeT3/src/br/maua/classes_dao/AnimeDAO.java
@@ -1,4 +1,64 @@
 package br.maua.classes_dao;
 
-public class AnimeDAO {
+import br.maua.dao.DAO;
+import br.maua.dao.DAOFields;
+import br.maua.model.Anime;
+
+import java.util.List;
+
+public class AnimeDAO implements DAO<Anime>, DAOFields {
+    @Override
+    public List<Anime> get(String condition) {
+        return null;
+    }
+
+    @Override
+    public List<Anime> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Anime anime) {
+
+    }
+
+    @Override
+    public void delete(Anime anime) {
+
+    }
+
+    @Override
+    public void create(Anime anime) {
+
+    }
+
+    @Override
+    public String getTableName() {
+        return null;
+    }
+
+    @Override
+    public String getDeleteString(String table) {
+        return null;
+    }
+
+    @Override
+    public String getUpdateString(String table) {
+        return null;
+    }
+
+    @Override
+    public String getInsertString(String tabel) {
+        return null;
+    }
+
+    @Override
+    public String getSelectAllString(String table) {
+        return null;
+    }
+
+    @Override
+    public String getSelectConditionalString(String table) {
+        return null;
+    }
 }
