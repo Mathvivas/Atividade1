cc5c94bd7597742b5ddb7ba107c47c3301c8209f
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-09-21 12:34:53 -0300

    Update API

--- AtividadeT3/src/br/maua/api/API.java
+++ AtividadeT3/src/br/maua/api/API.java
@@ -1,4 +1,19 @@
 package br.maua.api;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class API {
+
+    public void LeituraAPI(String nome) throws Exception {
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(
+                "https://api.jikan.moe/v3/search/anime?q=" + nome)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Recebidos");
+        System.out.println(response.body());
+    }
 }
