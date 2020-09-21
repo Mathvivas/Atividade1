413f7e8155fa5cc5cae2d1ce18ee19de881a6c2a
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-09-21 15:09:11 -0300

    Update Main

--- AtividadeT3/src/br/maua/Main.java
+++ AtividadeT3/src/br/maua/Main.java
@@ -1,8 +1,12 @@
 package br.maua;
 
+import br.maua.model.Sistema;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Sistema sistema = new Sistema();
+
+        sistema.run();
     }
 }
