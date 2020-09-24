60176046b2dd1ea3a3648ecdba9504f30042d28b
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-09-24 16:25:51 -0300

    Update ArrayParser

--- AtividadeT3/src/br/maua/json/ArrayParser.java
+++ AtividadeT3/src/br/maua/json/ArrayParser.java
@@ -1,4 +1,27 @@
 package br.maua.json;
 
+import br.maua.model.Anime;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayParser {
+
+    public static List<Anime> parseJson(String json) {
+        JSONObject retorno = new JSONObject(json);
+        JSONArray animesJson = retorno.getJSONArray("results");
+        List<Anime> animes = new ArrayList<>();
+
+        for (Object anime: animesJson) {
+            animes.add(new Anime(
+                    ((JSONObject)anime).getString("title"),
+                    ((JSONObject)anime).getString("image_url"),
+                    ((JSONObject)anime).getString("synopsis"),
+                    Integer.parseInt(((JSONObject)anime).getString("episodes")),
+                            Integer.parseInt(((JSONObject)anime).getString("score"))));
+        }
+        return animes;
+    }
 }
