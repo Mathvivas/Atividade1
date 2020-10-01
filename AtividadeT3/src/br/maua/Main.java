ee9397e6e27a864e904bad13cf7eb07c694152b7
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-10-01 16:08:31 -0300

    Documentação Main

--- AtividadeT3/src/br/maua/Main.java
+++ AtividadeT3/src/br/maua/Main.java
@@ -2,6 +2,14 @@ package br.maua;
 
 import br.maua.model.Sistema;
 
+/**
+ * Classe de inicialização do sistema.
+ * @author Gabriel de Godoy Braz - 17.00163-3
+ * @author Matheus Lopes Vivas - 17.04401-4
+ * @since 08/06/2020
+ * @version 1.0
+ */
+
 public class Main {
 
     public static void main(String[] args) {
