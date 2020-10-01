b5ce0aea4b2f2c68c6ef9012b9523eb56677cb93
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-10-01 16:25:04 -0300

    Documentação Interfaces

--- AtividadeT3/src/br/maua/dao/DAO.java
+++ AtividadeT3/src/br/maua/dao/DAO.java
@@ -2,6 +2,12 @@ package br.maua.dao;
 
 import java.util.List;
 
+/**
+ * Interface de tipo genérico que possui métodos que retornam listas.
+ * @author Gabriel de Godoy Braz - 17.00163-3
+ * @author Matheus Lopes Vivas - 17.04401-4
+ */
+
 public interface DAO <T>{       // Qualquer tipo --> Genérica
     List<T> get(String valor);
     List<T> getAll();
--- AtividadeT3/src/br/maua/dao/DAOFields.java
+++ AtividadeT3/src/br/maua/dao/DAOFields.java
@@ -1,5 +1,11 @@
 package br.maua.dao;
 
+/**
+ * Interface que possui os métodos de ligação com o Banco de Dados.
+ * @author Gabriel de Godoy Braz - 17.00163-3
+ * @author Matheus Lopes Vivas - 17.04401-4
+ */
+
 public interface DAOFields {
     String getTableName();
     String getInsertString(String table);
