d44b76db1e93ad910a2d48c18b14bc210bdc7f54
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-25 11:49:04 -0300

    Update Aplicação
    - Criação da função Menu.

--- RPG/src/sistema/Aplicacao.kt
+++ RPG/src/sistema/Aplicacao.kt
@@ -12,6 +12,39 @@ class Aplicacao {
         personagemDAO = PersonagemDAO()
     }
 
-    
+    fun run() {
+
+        var alive = true
+
+        do {
+            menu()
+            var op = Integer.parseInt(readLine())
+
+            when (op) {
+                0 -> alive = false
+                1 -> exibirPersonagens()
+                2 -> deletarPersonagem()
+                3 -> atualizarPersonagem()
+                4 -> cadastrarNovoPersonagem()
+                else -> println("Opção Inválida!")
+            }
+        } while (alive)
+    }
+
+
+    fun menu() {
+        println("|---------------------------------")
+        println("|")
+        println("| 1 - Personagens Cadastrados")
+        println("|")
+        println("| 2 - Deletar Personagem")
+        println("|")
+        println("| 3 - Alterar Personagem")
+        println("|")
+        println("| 4 - Cadastrar Personagem")
+        println("|")
+        println("| 0 - Sair")
+        println("|---------------------------------")
+    }
 
 }
