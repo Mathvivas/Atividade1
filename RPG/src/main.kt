03ed88bffed9e87742f5765982c6d7ce7fee464a
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-25 14:05:35 -0300

    Primeiro Teste

--- RPG/src/main.kt
+++ RPG/src/main.kt
@@ -1,7 +1,11 @@
+import sistema.Aplicacao
+
 // create table Personagem(id Integer primary key AUTOINCREMENT, nome varchar(20), raça varchar(20), profissão varchar(20),
 //                       mana integer, ataque integer, ataque_mágico integer, defesa integer, defesa_mágica integer,
 //                       velocidade integer, destreza integer, xp integer, nível integer);
 
 fun main() {
+    val aplicacao = Aplicacao()
 
+    aplicacao.run()
 }
