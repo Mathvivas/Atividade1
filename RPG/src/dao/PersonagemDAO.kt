39a25dec160ec361add36b964f4be94b259caf26
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-25 10:08:34 -0300

    Update PersonagemDAO

--- RPG/src/dao/PersonagemDAO.kt
+++ RPG/src/dao/PersonagemDAO.kt
@@ -18,25 +18,108 @@ class PersonagemDAO : DAO<Personagem>, DAOFields {
         }
     }
 
+
     override fun get(condition: String): List<Personagem> {
-        TODO("Not yet implemented")
+        var personagens : List<Personagem> = ArrayList()
+        try {
+            val statement : Statement = conn!!.createStatement()
+            val result : ResultSet = statement.executeQuery(getSelectConditionalString(getTableName() + condition))
+
+            while (result.next()) {
+                val personagem : Personagem = Personagem(
+                    result.getString("nome"),
+                result.getString("raça"), result.getString("profissão"), result.getInt("mana"),
+                result.getInt("ataque"), result.getInt("ataque_mágico"), result.getInt("defesa"),
+                result.getInt("defesa_mágica"), result.getInt("velocidade"), result.getInt("destreza"),
+                result.getInt("xp"), result.getInt("nível")
+                )
+                personagens.plus(personagem)
+            }
+            result.close()
+
+        } catch (e : Exception) {
+            e.printStackTrace()
+        }
+        return personagens
     }
 
     override fun getAll(): List<Personagem> {
-        TODO("Not yet implemented")
+        var personagens : List<Personagem> = ArrayList()
+        try {
+            val statement : Statement = conn!!.createStatement()
+            val result : ResultSet = statement.executeQuery(getSelectAllString(getTableName()))
+
+            while (result.next()) {
+                val personagem : Personagem = Personagem(
+                    result.getString("nome"),
+                    result.getString("raça"), result.getString("profissão"), result.getInt("mana"),
+                    result.getInt("ataque"), result.getInt("ataque_mágico"), result.getInt("defesa"),
+                    result.getInt("defesa_mágica"), result.getInt("velocidade"), result.getInt("destreza"),
+                    result.getInt("xp"), result.getInt("nível")
+                )
+                personagens.plus(personagem)
+            }
+            result.close()
+
+        } catch (e : Exception) {
+            e.printStackTrace()
+        }
+        return personagens
     }
 
-    override fun update(t: Personagem) {
-        TODO("Not yet implemented")
+    override fun update(personagem : Personagem) {
+        try {
+            val preparedStatement : PreparedStatement = conn!!.prepareStatement(getUpdateString(getTableName()))
+            preparedStatement.setString(1, personagem.nome.toString())
+            preparedStatement.setString(2, personagem.raça.toString())
+            preparedStatement.setString(3, personagem.profissão.toString())
+            preparedStatement.setString(4, personagem.mana.toString())
+            preparedStatement.setString(5, personagem.ataque.toString())
+            preparedStatement.setString(6, personagem.ataque_mágico.toString())
+            preparedStatement.setString(7, personagem.defesa.toString())
+            preparedStatement.setString(8, personagem.defesa_mágica.toString())
+            preparedStatement.setString(9, personagem.velocidade.toString())
+            preparedStatement.setString(10, personagem.destreza.toString())
+            preparedStatement.setString(11, personagem.xp.toString())
+            preparedStatement.setString(12, personagem.nível.toString())
+            preparedStatement.setString(13, personagem.nome.toString())
+
+
+            val retorno : Int = preparedStatement.executeUpdate()
+
+        } catch (e : Exception) {
+            e.printStackTrace()
+        }
     }
 
-    override fun create(t: Personagem) {
-        TODO("Not yet implemented")
+    override fun create(personagem: Personagem) {
+        try {
+            val preparedStatement : PreparedStatement = conn!!.prepareStatement(getUpdateString(getTableName()))
+            preparedStatement.setString(1, personagem.nome.toString())
+            preparedStatement.setString(2, personagem.raça.toString())
+            preparedStatement.setString(3, personagem.profissão.toString())
+            preparedStatement.setString(4, personagem.mana.toString())
+            preparedStatement.setString(5, personagem.ataque.toString())
+            preparedStatement.setString(6, personagem.ataque_mágico.toString())
+            preparedStatement.setString(7, personagem.defesa.toString())
+            preparedStatement.setString(8, personagem.defesa_mágica.toString())
+            preparedStatement.setString(9, personagem.velocidade.toString())
+            preparedStatement.setString(10, personagem.destreza.toString())
+            preparedStatement.setString(11, personagem.xp.toString())
+            preparedStatement.setString(12, personagem.nível.toString())
+
+            val retorno : Int = preparedStatement.executeUpdate()
+
+        } catch (e : Exception) {
+            e.printStackTrace()
+        }
     }
 
-    override fun delete(t: Personagem) {
+    override fun delete(personagem: Personagem) {
         try {
             val statement : PreparedStatement = conn!!.prepareStatement(getDeleteString(getTableName()))
+            statement.setString(1, personagem.nome.toString())
+            statement.executeUpdate()
 
         } catch (e : Exception) {
             e.printStackTrace()
@@ -46,11 +129,12 @@ class PersonagemDAO : DAO<Personagem>, DAOFields {
     override fun getTableName(): String { return "Personagem" }
 
     override fun getDeleteString(table: String): String {
-        return "DELETE FROM " + table + "WHERE id = ?;"
+        return "DELETE FROM " + table + "WHERE nome = ?;"
     }
 
     override fun getUpdateString(table: String): String {
-        TODO("Not yet implemented")
+         return "UPDATE " + table + " SET nome = ?, raça = ?, profissão = ?, ataque = ?, ataque_mágico = ?," +
+                 "defesa = ?, defesa_mágica = ?, velocidade = ?, destreza = ?, xp = ?, nível = ? WHERE nome = ?;"
     }
 
     override fun getInsertString(table: String): String {
