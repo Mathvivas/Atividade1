6b08f28ebed16f4178777e19bcec8a75015a0c28
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-30 10:28:19 -0300

    Javadoc Main

--- RPG/src/main.kt
+++ RPG/src/main.kt
@@ -4,6 +4,14 @@ import sistema.Aplicacao
 //                       mana integer, ataque integer, ataque_mágico integer, defesa integer, defesa_mágica integer,
 //                       velocidade integer, destreza integer, xp integer, nível integer);
 
+/**
+ * Classe de inicialização do sistema.
+ * @author Gabriel de Godoy Braz - 17.00163-3
+ * @author Matheus Lopes Vivas - 17.04401-4
+ * @since 28/11/2020
+ * @version 1.0
+ */
+
 fun main() {
     val aplicacao = Aplicacao()
 
