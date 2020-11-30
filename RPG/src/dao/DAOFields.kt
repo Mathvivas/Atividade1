97e5b448e35ad34d5cbdfa7c3aea3789ffff4ed9
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-30 10:31:44 -0300

    Javadoc DAOFields

--- RPG/src/dao/DAOFields.kt
+++ RPG/src/dao/DAOFields.kt
@@ -1,5 +1,11 @@
 package dao
 
+/**
+ * Interface que possui os métodos de ligação com o Banco de Dados.
+ * @author Gabriel de Godoy Braz - 17.00163-3
+ * @author Matheus Lopes Vivas - 17.04401-4
+ */
+
 interface DAOFields {
 
     fun getTableName() : String
