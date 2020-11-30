3c8a3ffbbdc36b2f0268e11428c910818a1c0473
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-30 10:32:18 -0300

    Javadoc DAO

--- RPG/src/dao/DAO.kt
+++ RPG/src/dao/DAO.kt
@@ -1,5 +1,11 @@
 package dao
 
+/**
+ * Interface de tipo genérico que possui métodos que retornam listas.
+ * @author Gabriel de Godoy Braz - 17.00163-3
+ * @author Matheus Lopes Vivas - 17.04401-4
+ */
+
 interface DAO <T>{
 
     fun get(condition : String) : List<T>
