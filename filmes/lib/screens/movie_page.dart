3eb15948ebaa1ea56b60dc710df4ff4e0abf9586
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-30 11:41:47 -0300

    Update movie_page

--- filmes/lib/screens/movie_page.dart
+++ filmes/lib/screens/movie_page.dart
@@ -1,11 +1,103 @@
+import 'package:flutter/material.dart';
+
 class MoviePage extends StatefulWidget {
   @override
   _MoviePageState createState() => _MoviePageState();
 }
 
 class _MoviePageState extends State<MoviePage> {
+  final _controller = MovieController();
+  final _scrollController = ScrollController();
+  int lastPage = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _initScrollListener();
+    _initialize();
+  }
+
+  _initScrollListener() {
+    _scrollController.addListener(() async {
+      if (_scrollController.offset >=
+      _scrollController.position.maxScrollExtent) {
+        if (_controller.currentPage == lastPage) {
+          lastPage++;
+          await _controller.fetchAllMovies(page: lastPage);
+          setState(() {});
+        }
+      }
+    });
+  }
+
+  _initialize() async {
+    setState(() {
+      _controller.loading = true;
+    });
+
+    await _controller.fetchAllMovies(page: lastPage);
+
+    setState(() {
+      _controller.loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildMovieGrid(),
+    );
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      title: Text(appName),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: _initialize,
+        ),
+      ],
+    );
+  }
+
+  _buildMovieGrid() {
+    if (_controller.loading) {
+      return CenteredProgress();
+    }
+
+    if (_controller.movieError != null) {
+      return CenteredMessage(message: _controller.movieError.message);
+    }
+
+    return GridView.builder(
+      controller: _scrollController,
+      padding: const EdgeInsets.all(2.0)
+      itemCount: _controller.movieCount,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 2,
+        childAspectRatio: 0.65,
+      ),
+      itemBuilder: _buildMovieCard,
+    );
+  }
+
+  Widget _buildMovieCard(context, index) {
+    final movie = _controller.movies[index];
+    return MovieCard(
+      posterPath: movie.posterPath,
+      onTap: () => _openDetailPage(movie.id)
+    );
+  }
+
+  _openDetailPage(movieId) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MovieDetailPage(movieId),
+        ),
+    );
   }
 }
