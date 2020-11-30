a25689b471b564ba97e7ebaef6f293ea5f266f14
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-30 11:22:14 -0300

    Primeira Página

---
+++ filmes/lib/screens/movie_page.dart
@@ -0,0 +1,11 @@
+class MoviePage extends StatefulWidget {
+  @override
+  _MoviePageState createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
