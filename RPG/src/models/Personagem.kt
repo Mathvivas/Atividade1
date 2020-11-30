1002011b582572483a1169ac0d31488a1bbe74c8
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-30 10:39:36 -0300

    Javadoc Personagem

--- RPG/src/models/Personagem.kt
+++ RPG/src/models/Personagem.kt
@@ -1,7 +1,13 @@
 package models
 
-class Personagem(nome: String, raça: String, profissão: String, mana: Int?, ataque: Int?, ataque_mágico: Int?,
-                 defesa: Int?, defesa_mágica: Int?, velocidade: Int?, destreza: Int?, xp: Int?, nível: Int?) {
+/**
+ * Classe responsável por representar o Personagem de RPG.
+ * @author Gabriel de Godoy Braz - 17.00163-3
+ * @author Matheus Lopes Vivas - 17.04401-4
+ */
+
+class Personagem(nome: String, raça: String, profissão: String, mana: Int?, ataque: Int?, ataqueMagico: Int?,
+                 defesa: Int?, defesaMagica: Int?, velocidade: Int?, destreza: Int?, xp: Int?, nível: Int?) {
 
     // Atributos da Classe
     var nome : String
@@ -9,13 +15,29 @@ class Personagem(nome: String, raça: String, profissão: String, mana: Int?, at
     var profissão : String
     var mana : Int
     var ataque : Int
-    var ataque_mágico : Int
+    var ataqueMagico : Int
     var defesa : Int
-    var defesa_mágica : Int
+    var defesaMagica : Int
     var velocidade : Int
     var destreza : Int
     var xp : Int
     var nível : Int
+
+    /**
+     *
+     * @param nome Nome do Personagem
+     * @param raça Raça do Personagem
+     * @param profissão Profissão do Personagem
+     * @param mana Quantidade de Mana do Personagem
+     * @param ataque Dano de ataque do Personagem
+     * @param ataqueMagico Dano de magia do Personagem
+     * @param defesa Defesa do Personagem
+     * @param defesaMagica Defesa Mágica do Personagem
+     * @param velocidade Velocidade do Personagem
+     * @param destreza Destreza do Personagem
+     * @param xp Quantidade de Experiência do Personagem
+     * @param nível Nível Atual do Personagem
+     */
 
     // Corpo do Construtor
     init {
@@ -24,9 +46,9 @@ class Personagem(nome: String, raça: String, profissão: String, mana: Int?, at
         this.profissão = profissão
         this.mana = mana!!
         this.ataque = ataque!!
-        this.ataque_mágico = ataque_mágico!!
+        this.ataqueMagico = ataqueMagico!!
         this.defesa = defesa!!
-        this.defesa_mágica = defesa_mágica!!
+        this.defesaMagica = defesaMagica!!
         this.velocidade = velocidade!!
         this.destreza = destreza!!
         this.xp = xp!!
@@ -39,7 +61,7 @@ class Personagem(nome: String, raça: String, profissão: String, mana: Int?, at
 
     override fun toString(): String {
         return "Personagem (nome = '$nome', raça = '$raça', profissão = '$profissão', mana = $mana, ataque = $ataque, " +
-                "ataque_mágico = $ataque_mágico, defesa = $defesa, defesa_mágica = $defesa_mágica, velocidade = $velocidade, " +
+                "ataque_mágico = $ataqueMagico, defesa = $defesa, defesa_mágica = $defesaMagica, velocidade = $velocidade, " +
                 "destreza = $destreza, xp = $xp, nível = $nível)"
     }
 
