b62ab0b125e3b2054a294e47a7a837e846b0a712
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-11-30 12:25:11 -0300

    Update movie_repository

---
+++ filmes/lib/repositories/movie_repository.dart
@@ -0,0 +1,38 @@
+class MovieRepository {
+  final Dio _dio = Dio(dioOptions);
+
+  Future<Either<MovieError, MovieResponseModel>> fetchAllMovies(
+      int page) async {
+    try {
+      final response = await _dio.get('/movie/popular?page=$page');
+      final model = MovieResponseModel.fromMap(response.data);
+      return Right(model);
+    } on DioError catch (error) {
+      if (error.response != null) {
+        return Left(
+            MovieRepositoryError(error.response.data['status_message']));
+      } else {
+        return Left(MovieRepositoryError(kServerError));
+      }
+    } on Exception catch (error) {
+      return Left(MovieRepositoryError(error.toString()));
+    }
+  }
+
+  Future<Either<MovieError, MovieDetailModel>> fetchMovieById(int id) async {
+    try {
+      final response = await _dio.get('/movie/$id');
+      final model = MovieDetailModel.fromMap(response.data);
+      return Right(model);
+    } on DioError catch (error) {
+      if (error.response != null) {
+        return Left(
+            MovieRepositoryError(error.response.data['status_message']));
+      } else {
+        return Left(MovieRepositoryError(kServerError));
+      }
+    } on Exception catch (error) {
+      return Left(MovieRepositoryError(error.toString()));
+    }
+  }
+}
